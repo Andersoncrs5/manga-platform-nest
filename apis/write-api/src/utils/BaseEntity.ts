01af91f9a0dba02b28d5892a51b5691ae5228a40
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-12 22:21:49 -0300

    Set up BaseEntity

---
+++ apis/write-api/src/utils/BaseEntity.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata';
+import {
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    VersionColumn
+} from 'typeorm';
+
+export abstract class BaseEntity {
+    @PrimaryGeneratedColumn({ unsigned: true })
+    id!: string;
+
+    @VersionColumn()
+    version!: number;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    createdAt!: Date;
+
+    @UpdateDateColumn({ type: 'timestamp' })
+    updatedAt: Date | undefined;
+}
