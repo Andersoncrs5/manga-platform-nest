3e1449a479635e8abc0f4c6c5aa792cfefefc0a3
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-23 19:58:56 -0300

    Set up PartialType

--- apis/write-api/src/user/dto/update-user.dto.ts
+++ apis/write-api/src/user/dto/update-user.dto.ts
@@ -1,6 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import {IsExistsUsername} from "../validations/existsUsername.validator";
+import {IsNotEmpty, IsString, Length} from "class-validator";
+import {Transform} from "class-transformer";
+import sanitizeHtml from "sanitize-html";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-    avatarUrl: string | null
+export class UpdateUserDto  {
+    name: string | null;
+    username: string | null;
+    password: string | null;
+    avatarUrl: string | null;
 }
