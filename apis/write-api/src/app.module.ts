2a758dcf1fc439e985fab80021c734483033fe42
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-23 19:56:41 -0300

    Added connection with db

--- apis/write-api/src/app.module.ts
+++ apis/write-api/src/app.module.ts
@@ -1,11 +1,44 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+import {addTransactionalDataSource, getDataSourceByName} from 'typeorm-transactional';
+import { User } from './user/entities/user.entity';
+import {UserModule} from "./user/user.module";
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USER'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          entities: [User],
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+      async dataSourceFactory(options) {
+        if (!options) throw new Error('Invalid options passed');
+
+        const existingInstance = getDataSourceByName('default');
+        if (existingInstance) return existingInstance;
+
+        return addTransactionalDataSource(new DataSource(options));
+      },
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
