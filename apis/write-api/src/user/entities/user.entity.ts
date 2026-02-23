9457cdfe3c93632a7aa77d5bc0a8185efcef9741
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-23 19:58:37 -0300

    improvements in definition to columns

--- apis/write-api/src/user/entities/user.entity.ts
+++ apis/write-api/src/user/entities/user.entity.ts
@@ -1,40 +1,40 @@
-import {BaseEntity} from "../../utils/BaseEntity";
-import {Column, Entity, Index} from "typeorm";
+import { BaseEntity } from "../../utils/BaseEntity";
+import { Column, Entity, Index } from "typeorm";
 
 @Entity('users')
 @Index('idx_username', ['username'])
 @Index('idx_email', ['email'])
 export class User extends BaseEntity {
-    @Column({ length: 100, nullable: false })
+    @Column({ type: 'varchar', length: 100, nullable: false })
     name: string;
 
-    @Column({ length: 100, unique: true, nullable: false })
+    @Column({ type: 'varchar', length: 100, unique: true, nullable: false })
     username: string;
 
-    @Column({ length: 150, unique: true, nullable: false })
+    @Column({ type: 'varchar', length: 150, unique: true, nullable: false })
     email: string;
 
-    @Column({ length: 300, nullable: false, select: false })
+    @Column({ type: 'varchar', length: 300, nullable: false, select: false })
     password: string;
 
     @Column({ type: 'boolean', default: true })
-    isActive: boolean = true;
+    isActive: boolean;
 
     @Column({ type: 'boolean', default: false })
-    isVerified: boolean = false;
+    isVerified: boolean;
 
-    @Column({ length: 50, nullable: true })
-    registrationIp: string | null = null;
+    @Column({ type: 'varchar', length: 50, nullable: true })
+    registrationIp: string | null;
 
-    @Column({ length: 600, nullable: true })
-    avatarUrl: string | null = null;
+    @Column({ type: 'varchar', length: 600, nullable: true })
+    avatarUrl: string | null;
 
-    @Column({ length: 500, nullable: true })
+    @Column({ type: 'varchar', length: 500, nullable: true })
     refreshToken: string | null;
 
     @Column({ type: 'timestamp', nullable: true })
     loginBlockAt: Date | null;
 
-    @Column({ type: 'smallint', nullable: true, default: 0 })
+    @Column({ type: 'smallint', default: 0 })
     attemptsLoginFailed: number;
 }
