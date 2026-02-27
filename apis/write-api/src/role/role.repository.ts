3388827e03c881e0978ad03f64020d032479864c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-26 22:06:54 -0300

    Set up RoleRepository

---
+++ apis/write-api/src/role/role.repository.ts
@@ -0,0 +1,20 @@
+import {Injectable} from "@nestjs/common";
+import {BaseRepository} from "../utils/BaseRepository";
+import {Role} from "./entities/role.entity";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+
+@Injectable()
+export class RoleRepository extends BaseRepository<Role> {
+    constructor(
+        @InjectRepository(Role)
+        private readonly roleRepository: Repository<Role>,
+    ) {
+        super(roleRepository);
+    }
+
+    async existsByName(name: string): Promise<boolean> {
+        return this.roleRepository.exists({ name } as any);
+    }
+
+}
