0852954a6d17f7619698a73496499bbc1c946f6c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-26 22:07:29 -0300

    Set up UserRoleRepository

---
+++ apis/write-api/src/user-role/user-role.repository.ts
@@ -0,0 +1,33 @@
+import {BaseRepository} from "../utils/BaseRepository";
+import {UserRole} from "./entities/user-role.entity";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {Injectable} from "@nestjs/common";
+
+@Injectable()
+export class UserRoleRepository extends BaseRepository<UserRole> {
+    constructor(
+        @InjectRepository(UserRole)
+        private readonly userRoleRepository: Repository<UserRole>,
+    ) {
+        super(userRoleRepository);
+    }
+
+    async findByUserIdAndRoleId(userId: string, roleId: string): Promise<UserRole | null> {
+        return await this.userRoleRepository.findOne({
+            where: {
+                userId,
+                roleId
+            }
+        })
+    }
+
+    async existsByUserIdAndRoleId(userId: string, roleId: string): Promise<boolean> {
+        return await this.userRoleRepository.exists({
+            where: {
+                userId,
+                roleId
+            }
+        });
+    }
+}
