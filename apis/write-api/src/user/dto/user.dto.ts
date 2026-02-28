bcdaedf54b21e914e807bf97a8a83cae1f2ab6ed
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-28 19:46:48 -0300

    new method parseToDto

--- apis/write-api/src/user/dto/user.dto.ts
+++ apis/write-api/src/user/dto/user.dto.ts
@@ -1,3 +1,5 @@
+import {User} from "../entities/user.entity";
+
 export class UserDto {
     id!: string;
     name: string;
@@ -7,6 +9,24 @@ export class UserDto {
     avatarUrl: string | null
     version!: number;
     loginBlockAt: Date | null
+    roles: string[] = []
     createdAt!: Date;
     updatedAt: Date | undefined;
+
+    static parseToDto(user: User): UserDto {
+        const dto = new UserDto();
+
+        dto.id = user.id;
+        dto.name = user.name;
+        dto.username = user.username;
+        dto.email = user.email;
+        dto.isActive = user.isActive;
+        dto.avatarUrl = user.avatarUrl;
+        dto.version = user.version;
+        dto.loginBlockAt = user.loginBlockAt;
+        dto.createdAt = user.createdAt;
+        dto.updatedAt = user.updatedAt;
+
+        return dto;
+    }
 }
