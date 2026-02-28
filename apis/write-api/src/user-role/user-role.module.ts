51e0756447bc299f39541e748c303b5954f843da
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-28 12:40:40 -0300

    set up UserRoleModule

---
+++ apis/write-api/src/user-role/user-role.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { UserRoleService } from './user-role.service';
+import { UserRoleController } from './user-role.controller';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UserRole} from "./entities/user-role.entity";
+import {UserRoleRepository} from "./user-role.repository";
+
+@Module({
+  imports: [
+      TypeOrmModule.forFeature([UserRole]),
+  ],
+  controllers: [UserRoleController],
+  providers: [
+      UserRoleService,
+      UserRoleRepository
+  ],
+  exports: [UserRoleService],
+})
+export class UserRoleModule {}
